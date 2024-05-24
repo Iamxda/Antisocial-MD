6d9042b80c4353831d35220d4dffbfe0e37db032
Iamxda <147917369+Iamxda@users.noreply.github.com>
2024-05-24 20:20:17 +0100

    Update general.js

--- Gallery/lib/general.js
+++ Gallery/lib/general.js
@@ -2,17 +2,17 @@ const prefix = global.prefa
   const pushname = m.pushName || "No Name"
 
 const generalmenu =`「 *Maria BOT* 」
-┌──⊰ _*🧧GENERAL🧧*_
-│⊳ 🌿 ${prefix}hi
-│⊳ 🌿 ${prefix}dev
-│⊳ 🌿 ${prefix}info
-│⊳ 🌿 ${prefix}support
-│⊳ 🌿 ${prefix}rules
-│⊳ 🌿 ${prefix}help
-│⊳ 🌿 ${prefix}runtime
-│⊳ 🌿 ${prefix}ping
-│⊳ 🌿 ${prefix}owner
-│⊳ 🌿 ${prefix}script
+┌──⊰ _*🕊GENERAL🕊*_
+│⊳ 🕊 ${prefix}bot
+│⊳ 🕊 ${prefix}dev
+│⊳ 🕊 ${prefix}info
+│⊳ 🕊 ${prefix}support
+│⊳ 🕊 ${prefix}rules
+│⊳ 🕊 ${prefix}help
+│⊳ 🕊 ${prefix}runtime
+│⊳ 🕊 ${prefix}ping
+│⊳ 🕊 ${prefix}owner
+│⊳ 🕊 ${prefix}script
 └──────────⊰
  `
  module.exports = generalmenu 
